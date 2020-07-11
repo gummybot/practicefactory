acd1004fadf8a258752e85b6010375bb75654e6b
gummybot <gurman.bhalla@gmail.com>
2020-07-12 04:47:01 +0530

    Adding Practice project

---
+++ Java/Practice/src/ClassMain.java
@@ -0,0 +1,11 @@
+public class ClassMain {
+    public static void main(String[] args) {
+    Vehicle jeep = new Vehicle();
+    jeep.model = "Jeep";
+    jeep.v_type = "Car";
+    jeep.no_seats = 5;
+    jeep.no_wheels = 4;
+    jeep.color = "Gray";
+    jeep.display();
+    }
+}
