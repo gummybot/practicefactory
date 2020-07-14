d3a5e9b9409bc23c15a99acc7b2600ecda21faee
gummybot <gurman.bhalla@gmail.com>
2020-07-14 08:01:00 +0530

    JFrame example

--- Java/Practice/src/ClassMain.java
+++ Java/Practice/src/ClassMain.java
@@ -1,3 +1,6 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class ClassMain {
     public static void main(String[] args) {
     Vehicle jeep = new Vehicle();
@@ -7,5 +10,14 @@ public class ClassMain {
     jeep.no_wheels = 4;
     jeep.color = "Gray";
     jeep.display();
+
+    JFrame window = new JFrame();                   //creating window variable to hold JFrame object
+    window.setSize(500,300);           //setting size of window
+    window.setTitle("JFrame Window");               //setting title of window
+    window.setVisible(true);                        //setting window visibility to true
+    JLabel label = new JLabel();                    //creating label variable to hold JLabel object
+    label.setText("Label");                         //setting text of label
+    window.add(label);                              //adding label to window
+
     }
 }
